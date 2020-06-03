06a40584c2ea42b0e934baf0cb21489e82d62c13
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-03 19:30:06 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 Bundler.require
 
-require_relative '../lib/student'
+require 'sqlite3'
+require_relative '../lib/student.rb'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
